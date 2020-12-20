14d0fc814c02f8cafef2d9ea391a32f5f992355b
bg-write <bradywgerber@gmail.com>
2020-12-19 23:24:27 -0500

    CSS: overall, round 2

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ router.get(
 router.get(
 	'/google/oauth2callback',
 	passport.authenticate('google', {
-		successRedirect: '/',
+		successRedirect: '/albums/new',
 		failureRedirect: '/auth/google',
 	})
 );
