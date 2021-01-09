2b3e59f80b904412a13035b439ad8e8df4c457ac
bg-write <bradywgerber@gmail.com>
2021-01-09 12:56:23 -0500

    Update message board guidance on errors

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/', function (req, res) {
+router.get('/', function (req, res, next) {
 	res.render('index', {
 		title: 'I Love That Song',
 		user: req.user ? req.user : null,
